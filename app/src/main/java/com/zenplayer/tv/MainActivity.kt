890afa784876cb2c20cbf61d3cf16b8dcc75785f
jayzenno <jsn.ykk@gmail.com>
2026-09-16 16:52:35 +0200

    Use dark TV theme for readable text inputs

--- app/src/main/java/com/zenplayer/tv/MainActivity.kt
+++ app/src/main/java/com/zenplayer/tv/MainActivity.kt
@@ -3,12 +3,27 @@ package com.zenplayer.tv
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ZenLogger.init(this)
         ZenLogger.info("APP", "MainActivity.onCreate")
-        setContent { ZenPlayerShellV3(SettingsStore(this)) }
+        setContent {
+            MaterialTheme(
+                colorScheme = darkColorScheme(
+                    primary = Color(0xFF70E6FF),
+                    onPrimary = Color(0xFF001018),
+                    surface = Color(0xFF0B0F18),
+                    onSurface = Color(0xFFF7F8FC),
+                    onSurfaceVariant = Color(0xFFB8C0D0)
+                )
+            ) {
+                ZenPlayerShellV3(SettingsStore(this))
+            }
+        }
     }
 }
