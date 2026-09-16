94687a5ac43f1c5e3de76a4bbb0115cabdd0d1aa
jayzenno <jsn.ykk@gmail.com>
2026-09-17 00:15:33 +0200

    fix TV back handling baseline

--- app/src/main/java/com/zenplayer/tv/MainActivity.kt
+++ app/src/main/java/com/zenplayer/tv/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zenplayer.tv
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.OnBackPressedDispatcher
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.ui.graphics.Color
