826bba8424fd60b36a0e7f6f82a2eeab46be6f92
jayzenno <jsn.ykk@gmail.com>
2026-09-16 13:44:44 +0200

    build: add Compose extended material icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.tv:tv-material:1.0.0")
     implementation("androidx.tv:tv-foundation:1.0.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.2")
