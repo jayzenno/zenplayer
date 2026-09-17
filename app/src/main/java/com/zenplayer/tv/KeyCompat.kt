b8b4c1e7a29af8b9fb854784e80a57b5a742dd6c
jayzenno <jsn.ykk@gmail.com>
2026-09-17 03:03:39 +0200

    fix Android TV key dispatch via preview events

--- app/src/main/java/com/zenplayer/tv/KeyCompat.kt
+++ app/src/main/java/com/zenplayer/tv/KeyCompat.kt
@@ -5,9 +5,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEvent
 import androidx.compose.ui.input.key.KeyEventType
-import androidx.compose.ui.input.key.onKeyEvent as foundationOnKeyEvent
+import androidx.compose.ui.input.key.onPreviewKeyEvent as foundationOnPreviewKeyEvent
 
-fun Modifier.onKeyEvent(handler: (KeyEvent) -> Boolean): Modifier = this.foundationOnKeyEvent(handler)
+fun Modifier.onKeyEvent(handler: (KeyEvent) -> Boolean): Modifier = this.foundationOnPreviewKeyEvent(handler)
 
 val KeyEvent.type: KeyEventType
     get() = when (nativeKeyEvent.action) {
